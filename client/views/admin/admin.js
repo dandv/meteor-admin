57ef7855c70aa1e003269d661c09879cfb048f30
Pent <jon@empire5design.com>
2013-09-13 12:31:03 -0600

    working commit

--- client/views/admin/admin.js
+++ client/views/admin/admin.js
@@ -1,12 +1,14 @@
-Template.admin.helpers({
-  plans: function() {
-    return Plans.find();
-  },
-  plansReady: function() {
-    return plansHandler.ready();
-  }
-});
+if (Meteor.isClient) {
+  Template.admin.helpers({
+    plans: function() {
+      return Plans.find();
+    },
+    plansReady: function() {
+      return plansHandler.ready();
+    }
+  });
 
-Template.admin.events({
+  Template.admin.events({
 
-});
+  });
+};
--- client/views/admin/adminHeader.js
+++
@@ -1,3 +0,0 @@
-Template.adminHeader.helpers({
-
-});
